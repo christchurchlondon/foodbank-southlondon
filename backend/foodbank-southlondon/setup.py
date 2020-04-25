b555321855dd65a5e5d139c7f7de3423479b5449
Adam Cunnington <adamcunnington.info@gmail.com>
2020-04-25 20:01:58 +0100

    Add base setup.py

---
+++ backend/foodbank-southlondon/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+with open("requirements.txt") as f:
+    dependencies = f.readlines()
+
+setuptools.setup(
+    name="foodbank-southlondon",
+    install_requires=dependencies,
+    extras_require={
+        "dev": [
+            "flake8",
+            "mypy"
+        ]
+    }
+)
