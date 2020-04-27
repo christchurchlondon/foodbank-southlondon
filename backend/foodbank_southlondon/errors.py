0594fc68544239b226db4087716fc7d01067abb7
Adam Cunnington <adamcunnington.info@gmail.com>
2020-04-28 00:00:08 +0100

    Improve error handlers

--- backend/foodbank_southlondon/errors.py
+++ backend/foodbank_southlondon/errors.py
@@ -1,8 +1,12 @@
 from typing import Tuple
 
+from werkzeug import exceptions
+import flask
+
 from foodbank_southlondon import app
 
 
-@app.errorhandler(404)
-def handle_404(error: Exception) -> Tuple[str, int]:
-    return ("This page does not exist", 404)
+@app.errorhandler(exceptions.HTTPException)
+def handle_http_error(error: exceptions.HTTPException) -> Tuple[str, int]:
+    flask.current_app.logger.debug(f"A {error.name} ({error.code}: {error.description}) error was encountered.")
+    return (error.name, error.code)
