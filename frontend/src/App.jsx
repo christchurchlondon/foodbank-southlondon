a15f94f6e504efd1e51fdbda4ec89d82c61a18f8
danieljones740 <danieljones740@gmail.com>
2020-05-02 11:31:01 +0100

    Cosmetic updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Main from './components/main';
-import './App.css';
 
 function App() {
     return <Main />;
--- frontend/src/components/requests/index.jsx
+++ frontend/src/components/requests/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { getRequestsState } from '../../redux/selectors';
 import { fetchRequests } from '../../redux/actions';
+import Loading from '../common/loading';
 
 class Requests extends React.Component {
     
@@ -15,12 +16,21 @@ class Requests extends React.Component {
         const requestItems = this.props.items
             .map(item => <p key={item}>{ item }</p>);
 
+        // TODO make constants file for statuses
+        const contents = this.props.status === 'loading'
+            ? <Loading />
+            : (
+                <div>
+                    <h2>Requests</h2>
+                    <p>TODO: Filters</p>
+                    { requestItems }
+                    <p>TODO: Actions</p>
+                </div>
+            );
+
         return (
-            <div>
-                <h2>Requests</h2>
-                <p>TODO: Filters</p>
-                { requestItems }
-                <p>TODO: Actions</p>
+            <div className="requests-container">
+                { contents }
             </div>
         );
     }
