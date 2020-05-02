40c636e808947bea5263ab0a06c419060a025878
Adam Cunnington <adamcunnington.info@gmail.com>
2020-05-02 20:42:26 +0100

    Add edit_details_url to request model

--- backend/foodbank_southlondon/views.py
+++ backend/foodbank_southlondon/views.py
@@ -1,9 +1,10 @@
 import flask
+import werkzeug
 
 from foodbank_southlondon import app
 
 
 @app.route("/", defaults={"path": ""})
 @app.route("/<path:path>")
-def catch_all(path: str) -> flask.Response:
+def catch_all(path: str) -> werkzeug.Response:
     return flask.redirect("http://localhost:3000")
