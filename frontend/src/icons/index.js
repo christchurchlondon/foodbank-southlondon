e5cd8ac587c66d9d494fea067c96541aa6927af5
danieljones740 <danieljones740@gmail.com>
2020-05-07 23:32:25 +0100

    Use shared colours in index.scss

--- frontend/src/icons/index.js
+++ frontend/src/icons/index.js
@@ -2,6 +2,8 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faComment } from '@fortawesome/free-solid-svg-icons'
 
+// Add more icons here when required
+
 export default function setupIcons() {
     library.add( faComment )
 }
