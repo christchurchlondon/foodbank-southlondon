40ee8b900d5e5f266063500a65365910367143c8
danieljones740 <danieljones740@gmail.com>
2020-05-30 11:04:06 +0100

    Use React routing rather than manually using tabs

--- frontend/src/components/header/index.jsx
+++ frontend/src/components/header/index.jsx
@@ -1,36 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import Tabs from './tabs';
-import { setTab } from '../../redux/actions';
-import { getTab } from '../../redux/selectors';
 import logo from '../../assets/logo.png';
 import './styles/index.scss';
 
-class Header extends React.Component {
-
-    // TODO remove state management from here?
-
-    handleTabSelect(tab) {
-        this.props.setTab(tab);
-    }
-
-    render() {
-        return (
-            <header className="header">
-                <img className="logo" src={logo} alt="Lambeth Foodbank" />
-                <Tabs onSelect={ tab => this.handleTabSelect(tab) } selected={ this.props.tab } />
-                <a className="logout" href="/logout">Log out</a>
-            </header>
-        );
-    };
+export default function Header() {
+    return (
+        <header className="header">
+            <img className="logo" src={logo} alt="Lambeth Foodbank" />
+            <Tabs />
+            <a className="logout" href="/logout">Log out</a>
+        </header>
+    );
 }
-
-const mapStateToProps = state => {
-    const tab = getTab(state);
-    return { tab };
-}
-
-export default connect(
-    mapStateToProps,
-    { setTab }
-)(Header);
