60f9bd8eb0893bda76d142f0117fe35675350691
Adam Cunnington <adamcunnington.info@gmail.com>
2020-06-06 23:24:00 +0100

    Replace delivery_dates param in /bff/status/ for start_date & end_date

--- backend/foodbank_southlondon/bff/parsers.py
+++ backend/foodbank_southlondon/bff/parsers.py
@@ -1,3 +1,5 @@
+import datetime
+
 from foodbank_southlondon.api import parsers
 from foodbank_southlondon.api.parsers import cache_params  # noqa: F401
 from foodbank_southlondon.api.requests.parsers import requests_params
@@ -7,7 +9,12 @@ _requests_params_args = {arg.name: arg for arg in requests_params.args}
 
 
 status_params = parsers.pagination_params.copy()
-status_params.add_argument(_requests_params_args["delivery_dates"])
+status_params.add_argument("start_date", type=lambda x: datetime.date.fromisoformat(x), required=False,
+                           help="An ISO 8601 formatted start date value to filter requests from. If an end_date is provided but a start_date is not "
+                           "provided, the start_date defaults to the first of the month.")
+status_params.add_argument("end_date", type=lambda x: datetime.date.fromisoformat(x), required=False,
+                           help="An ISO 8601 formatted end date value to filter requests until. If a start_date is provided but an end_date is not "
+                           "provided, it end_date defaults to to today.")
 status_params.add_argument(_requests_params_args["client_full_names"])
 status_params.add_argument(_requests_params_args["postcodes"])
 status_params.add_argument(_requests_params_args["reference_numbers"])
