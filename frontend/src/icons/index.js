93f0089b90f98531d0ec71bf9e277c40f0c1900f
danieljones740 <danieljones740@gmail.com>
2020-06-06 20:04:05 +0100

    Save icon

--- frontend/src/icons/index.js
+++ frontend/src/icons/index.js
@@ -14,10 +14,15 @@ import {
     faTimes,
     faUser
 } from '@fortawesome/free-solid-svg-icons'
+import {
+    faSave
+} from '@fortawesome/free-regular-svg-icons'
 
 // Add more icons here when required
 
 export default function setupIcons() {
+    
+    // Solid icons
     library.add(
         faPlus,
         faArrowDown,
@@ -32,4 +37,10 @@ export default function setupIcons() {
         faTimes,
         faUser
     );
+
+    // Regular icons
+    library.add(
+        faSave
+    );
+
 }
