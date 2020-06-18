383efd0ec25d7ae8f1bd384a8955dcca2b8746ef
Adam Cunnington <adamcunnington.info@gmail.com>
2020-06-18 19:52:15 +0100

    Add confirmation_label field to API response

--- backend/foodbank_southlondon/api/events/models.py
+++ backend/foodbank_southlondon/api/events/models.py
@@ -4,26 +4,29 @@ from foodbank_southlondon.api import models, rest
 from foodbank_southlondon.api.requests import models as requests_models
 
 
-EVENT_NAMES = (
-    "Deny Request",
-    "Print Shopping List",
-    "Print Shipping Label",
-    "Print Driver Overview",
-    "Mark as Called and Confirmed",
-    "Mark as Called and No Response",
-    "Mark as Out for Delivery",
-    "Mark as Delivered",
-    "Mark as Delivery Cancelled",
-    "Mark as Fullfilled with Trussell Trust",
-    "Permanently Delete Request"
-)
+EVENTS = {
+    "Deny Request": "This action marks the request as denied but does not delete the request from the system.",
+    "Print Shopping List": None,
+    "Print Shipping Label": "How many boxes have been made for this client?",
+    "Print Driver Overview": None,
+    "Mark as Called and Confirmed": "What date did you call the client?",
+    "Mark as Called and No Response": "What date did you attempt to call the client?",
+    "Mark as Out for Delivery": "What date did the parcels leave the warehouse?",
+    "Mark as Delivered": "What date were the parcels delivered?",
+    "Mark as Delivery Cancelled": "What date was the delivery marked as cancelled?",
+    "Mark as Fullfilled with Trussell Trust": "What date was Trussel Trust updated as fulfilled for this request?",
+    "Permanently Delete Request": "This action permanently deletes the data from the system. This action cannot be undone. Are you sure you wish to "
+                                  "continue?"
+}
 
+
+_example_name, _example_label = next(iter(EVENTS.items()))
 
 event = rest.model("Event", {
     "request_id": requests_models.request["request_id"],
     "event_timestamp": fields.DateTime(required=True, description="The timestamp when the event was recorded, in ISO 8601 format",
                                        example="2020-04-26T13:31:42Z"),
-    "event_name": fields.String(required=True, description="The name of the event that occured", example="Print Shipping Label", enum=EVENT_NAMES),
+    "event_name": fields.String(required=True, description="The name of the event that occured", example=_example_name, enum=list(EVENTS.keys())),
     "event_data": fields.String(required=True, description="The data recorded with the event (blank, a date or quantity)", example="2020-04-01")
 })
 
@@ -35,12 +38,14 @@ all_events = rest.model("AllEvents", {
     "items": fields.List(fields.Nested(event))
 })
 
+
 _distinct_event_name_value = rest.model("DistinctEventNameValue", {
-    "event_name": fields.String(required=True, description="A valid event_name value"),
+    "event_name": fields.String(required=True, description="A valid event_name value", example=_example_name),
     "confirmation_expected": fields.Boolean(required=True, description="Whether this event_name expects special confirmation.", example=False),
     "date_expected": fields.Boolean(required=True, description="Whether this event_name expects a date to be captured.", example=True),
     "quantity_expected": fields.Boolean(required=True, description="Whether this event_name expects a quantity to be captured.", example=True),
-    "returns_pdf": fields.Boolean(required=True, description="Whether a POST of this event_name would return a PDF or not.", example=False)
+    "returns_pdf": fields.Boolean(required=True, description="Whether a POST of this event_name would return a PDF or not.", example=False),
+    "confirmation_label": fields.String(required=True, description="The label to show when requesting confirmation.", example=_example_label)
 })
 
 distinct_event_name_values = rest.model("DistinctEventNameValues", {
--- backend/foodbank_southlondon/api/events/views.py
+++ backend/foodbank_southlondon/api/events/views.py
@@ -71,11 +71,12 @@ class DistinctEventNameValues(flask_restx.Resource):
         params = parsers.distinct_events_params.parse_args(flask.request)  # noqa: F841 - here as reminder; currently we only support 1 value
         values = [{
             "event_name": event_name,
-            "confirmation_expected": not event_name.startswith("Print"),
+            "confirmation_expected": not event_name.startswith("Print") or not event_name.endswith("Label"),
             "date_expected": event_name.startswith("Mark"),
             "quantity_expected": event_name.endswith("Label"),
-            "returns_pdf": event_name.startswith("Print")
-        } for event_name in models.EVENT_NAMES]
+            "returns_pdf": event_name.startswith("Print"),
+            "confirmation_label": event_label
+        } for event_name, event_label in models.EVENTS.items()]
         return {"values": values}
 
 
