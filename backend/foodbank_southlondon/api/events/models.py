4aaee7ea5e00d301e52d0a65fd3e7e2944be3974
Adam Cunnington <adamcunnington.info@gmail.com>
2020-11-05 15:31:03 +0000

    Refactor events into dataclasses

--- backend/foodbank_southlondon/api/events/models.py
+++ backend/foodbank_southlondon/api/events/models.py
@@ -1,47 +1,62 @@
+import dataclasses
+
 from flask_restx import fields  # type:ignore
 
 from foodbank_southlondon.api import models, rest
 from foodbank_southlondon.api.requests import models as requests_models
 
 
-STATUSES = {
-    "Request Denied": "This action marks the request as denied but does not delete the request from the system.",
-    "No Status": None,
-    "Pending More Info": None,
-    "Called & Confirmed": "What date did you call the client?",
-    "Called & No Response": "What date did you attempt to call the client?",
-    "Ready to Dispatch": None,
-    "Delivered": "What date were the parcels delivered?",
-    "Delivery Failed": "What date did the delivery fail?",
-    "Client Contact Required": None,
-    "Delivery Cancelled": "What date was the delivery marked as cancelled?",
-    "Fullfilled with Trussell Trust": "What date was Trussel Trust updated as fulfilled for this request?",
-    "Request Deleted": None
-}
+@dataclasses.dataclass
+class Event:
+    name: str
+    confirmation_expected: bool
+    date_expected: bool
+    quantity_expected: bool
+    name_expected: bool
+    returns_pdf: bool
+    confirmation_label: str = None
+    value_override: str = None
 
-ACTIONS = {
-    "Print Shopping List": None,
-    "Print Shipping Label": "How many boxes have been made for this client?",
-    "Print Driver Overview": None,
-    "Permanently Delete Request": "This action permanently deletes the data from the system. This action cannot be undone. Are you sure you wish to "
-                                  "continue?"
-}
 
-ACTION_STATUSES = (
-    "Shopping List Printed",
-    "Shipping Label Printed",
-    "Driver Overview Printed"
+STATUSES = (
+    Event("Request Denied", True, False, False, False, False, "This action marks the request as denied but does not delete the request."),
+    Event("No Status", True, False, False, False, False, "This action clears the current status.", ""),
+    Event("Pending More Info", True, False, False, False, False),
+    Event("Called & Confirmed", True, True, False, False, False, "What date did you call the client?"),
+    Event("Called & No Response", True, True, False, False, False, "What date did you attempt to call the client?"),
+    Event("Ready to Dispatch", True, False, False, False, False),
+    Event("Delivered", True, True, False, False, False, "What date were the parcels delivered?"),
+    Event("Delivery Failed", True, True, False, False, False, "What date did the delivery fail?"),
+    Event("Client Contact Required", True, False, False, False, False),
+    Event("Delivery Cancelled", True, True, False, False, False, "What date was the delivery marked as cancelled?"),
+    Event("Fullfilled with Trussell Trust", True, True, False, False, False, "What date was Trussel Trust updated as fulfilled for this request?")
 )
 
-EVENTS = list(STATUSES) + list(ACTION_STATUSES)
+ACTIONS = (
+    Event("Print Shopping List", False, False, False, False, True),
+    Event("Print Shipping Label", True, False, True, False, True, "How many boxes have been made for this client?"),
+    Event("Print Driver Overview", True, False, False, True, True),
+    Event("Permanently Delete Request", True, False, False, False, False, "This action permanently deletes the request. This action cannot be "
+          "undone.Are you sure you wish to continue?")
+)
 
-_example_name, _example_label = next(iter(STATUSES.items()))
+ACTION_STATUSES = (
+    Event("Shopping List Printed", False, False, False, False, False),
+    Event("Shipping Label Printed", False, False, False, False, False),
+    Event("Driver Overview Printed", False, False, False, False, False),
+    Event("Request Deleted", False, False, False, False, False)
+)
+
+EVENTS = STATUSES + ACTION_STATUSES
+
+_example_event = EVENTS[0]
 
 event = rest.model("Event", {
     "request_id": requests_models.request["request_id"],
     "event_timestamp": fields.DateTime(required=True, description="The timestamp when the event was recorded, in ISO 8601 format",
                                        example="2020-04-26T13:31:42Z"),
-    "event_name": fields.String(required=True, description="The name of the event that occured", example=_example_name, enum=EVENTS),
+    "event_name": fields.String(required=True, description="The name of the event that occured", example=_example_event.name,
+                                enum=[event.name for event in EVENTS]),
     "event_data": fields.String(required=True, description="Data that accompanied the event submission such as a date or name", example="2020-04-01")
 })
 
@@ -53,16 +68,17 @@ all_events = rest.model("AllEvents", {
     "items": fields.List(fields.Nested(event))
 })
 
-_distinct_event_name_value = rest.model("DistinctEventNameValue", {
-    "event_name": fields.String(required=True, description="A valid event_name value", example=_example_name),
+_distinct_event_type = rest.model("DistinctEventType", {
+    "event_name": fields.String(required=True, description="A valid event_name value", example=_example_event.name),
     "confirmation_expected": fields.Boolean(required=True, description="Whether this event_name expects special confirmation.", example=False),
     "date_expected": fields.Boolean(required=True, description="Whether this event_name expects a date to be captured.", example=True),
     "quantity_expected": fields.Boolean(required=True, description="Whether this event_name expects a quantity to be captured.", example=True),
     "name_expected": fields.Boolean(required=True, description="Whether this event_name expects a driver name to be captured.", example=False),
     "returns_pdf": fields.Boolean(required=True, description="Whether a POST of this event_name would return a PDF or not.", example=False),
-    "confirmation_label": fields.String(required=True, description="The label to show when requesting confirmation.", example=_example_label)
+    "confirmation_label": fields.String(required=True, description="The label to show when requesting confirmation.",
+                                        example=_example_event.confirmation_label)
 })
 
-distinct_event_name_values = rest.model("DistinctEventNameValues", {
-    "values": fields.List(fields.Nested(_distinct_event_name_value))
+distinct_event_types = rest.model("DistinctEventTypes", {
+    "items": fields.List(fields.Nested(_distinct_event_type))
 })
