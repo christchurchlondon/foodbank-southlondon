86af0807031433e97b7895f0d2223d4a42ef8272
Adam Cunnington <adamcunnington.info@gmail.com>
2020-11-05 14:43:30 +0000

    Refactor events models definition and adjust parsers

--- backend/foodbank_southlondon/api/events/models.py
+++ backend/foodbank_southlondon/api/events/models.py
@@ -4,30 +4,45 @@ from foodbank_southlondon.api import models, rest
 from foodbank_southlondon.api.requests import models as requests_models
 
 
-EVENTS = {
-    "Deny Request": "This action marks the request as denied but does not delete the request from the system.",
+STATUSES = {
+    "Request Denied": "This action marks the request as denied but does not delete the request from the system.",
+    "No Status": None,
+    "Pending More Info": None,
+    "Called & Confirmed": "What date did you call the client?",
+    "Called & No Response": "What date did you attempt to call the client?",
+    "Ready to Dispatch": None,
+    "Delivered": "What date were the parcels delivered?",
+    "Delivery Failed": "What date did the delivery fail?",
+    "Client Contact Required": None,
+    "Delivery Cancelled": "What date was the delivery marked as cancelled?",
+    "Fullfilled with Trussell Trust": "What date was Trussel Trust updated as fulfilled for this request?",
+    "Request Deleted": None
+}
+
+ACTIONS = {
     "Print Shopping List": None,
     "Print Shipping Label": "How many boxes have been made for this client?",
     "Print Driver Overview": None,
-    "Mark as Called and Confirmed": "What date did you call the client?",
-    "Mark as Called and No Response": "What date did you attempt to call the client?",
-    "Mark as Out for Delivery": "What date did the parcels leave the warehouse?",
-    "Mark as Delivered": "What date were the parcels delivered?",
-    "Mark as Delivery Cancelled": "What date was the delivery marked as cancelled?",
-    "Mark as Fullfilled with Trussell Trust": "What date was Trussel Trust updated as fulfilled for this request?",
     "Permanently Delete Request": "This action permanently deletes the data from the system. This action cannot be undone. Are you sure you wish to "
                                   "continue?"
 }
 
+ACTION_STATUSES = (
+    "Shopping List Printed",
+    "Shipping Label Printed",
+    "Driver Overview Printed"
+)
 
-_example_name, _example_label = next(iter(EVENTS.items()))
+EVENTS = list(STATUSES) + list(ACTION_STATUSES)
+
+_example_name, _example_label = next(iter(STATUSES.items()))
 
 event = rest.model("Event", {
     "request_id": requests_models.request["request_id"],
     "event_timestamp": fields.DateTime(required=True, description="The timestamp when the event was recorded, in ISO 8601 format",
                                        example="2020-04-26T13:31:42Z"),
-    "event_name": fields.String(required=True, description="The name of the event that occured", example=_example_name, enum=list(EVENTS.keys())),
-    "event_data": fields.String(required=True, description="The data recorded with the event (blank, a date or quantity)", example="2020-04-01")
+    "event_name": fields.String(required=True, description="The name of the event that occured", example=_example_name, enum=EVENTS),
+    "event_data": fields.String(required=True, description="Data that accompanied the event submission such as a date or name", example="2020-04-01")
 })
 
 page_of_events = rest.inherit("EventsPage", models.pagination, {
@@ -37,7 +52,6 @@ page_of_events = rest.inherit("EventsPage", models.pagination, {
 all_events = rest.model("AllEvents", {
     "items": fields.List(fields.Nested(event))
 })
-
 
 _distinct_event_name_value = rest.model("DistinctEventNameValue", {
     "event_name": fields.String(required=True, description="A valid event_name value", example=_example_name),
