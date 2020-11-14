57e7e38be872aa3ef959684eea81b03a1c2e8609
danieljones740 <danieljones740@gmail.com>
2020-11-14 09:19:04 +0000

    New styling for actions/statuses

--- frontend/src/components/requests/actions.jsx
+++ frontend/src/components/requests/actions.jsx
@@ -25,17 +25,14 @@ export default class RequestsActions extends React.Component {
             : 'No clients selected';
 
         return (
-            <div className="requests-actions">
-                <div className="panel">
-                    <Menu options={statusOptions}
-                        label={label}
-                        disabled={ !this.props.recordCount } />
-                </div>
-                <div className="panel">
-                    <Menu options={actionOptions}
-                        label={label}
-                        disabled={ !this.props.recordCount } />
-                </div>
+            <div className="requests-actions panel">
+                <label>{ label }</label>
+                <Menu options={statusOptions}
+                    label="Statuses"
+                    disabled={ !this.props.recordCount } />
+                <Menu options={actionOptions}
+                    label="Actions"
+                    disabled={ !this.props.recordCount } />
             </div>
         );
     }
