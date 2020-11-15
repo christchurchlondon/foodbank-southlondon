0b54f0d3abf87f895c4e5ca900759c2f25e3b731
danieljones740 <danieljones740@gmail.com>
2020-11-15 12:10:43 +0000

    New flag component

---
+++ frontend/src/components/requests/flag.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import './styles/flag.scss';
+
+export default function Flag(props) {
+    return <Icon icon="flag" className="flag" title="Flagged for action" />;
+}
