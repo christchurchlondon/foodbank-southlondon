d947517770cccab9604beb26ce22e20110cd63f2
Adam Cunnington <adamcunnington.info@gmail.com>
2021-01-20 22:35:37 +0000

    Fix typo

--- backend/foodbank_southlondon/api/events/parsers.py
+++ backend/foodbank_southlondon/api/events/parsers.py
@@ -8,4 +8,4 @@ events_params = pagination_params.copy()
 events_params.add_argument("request_ids", type=str, required=False, action="split", help="A comma separated list of request_id values to filter on")
 events_params.add_argument("event_names", type=str, required=False, action="split", help="An event name to filter results to.")
 events_params.add_argument("latest_event_only", type=inputs.boolean, required=False, help="Whether only the latest event (based on Timestamp) "
-                           "should be returned, per request_id - the event_name filter is applied first if passed")
+                           "should be returned, per request_id - the event_names filter is applied first if passed")
